5f878535cc1b7ffede3e6ea1a1c8c53f92eb2cf3
Vadim Gorbachev <vadim.gorbachev.main@gmail.com>
2019-11-09 19:19:25 +0300

    init commit

--- test3/ViewController.swift
+++ test3/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let cellId = "id"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
-
+    
+    // MARK: override tableView methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 16
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = "OKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOK"
+        cell.textLabel?.numberOfLines = 0
+        
+        
+        return cell
+    }
+    
 
 }
 
