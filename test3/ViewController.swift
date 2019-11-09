35d69b7040617ee3a0d82a3083fcc5def84aa7fb
Vadim Gorbachev <vadim.gorbachev.main@gmail.com>
2019-11-09 18:53:27 +0300

    Initial Commit

---
+++ test3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test3
+//
+//  Created by Vadim  Gorbachev on 09.11.2019.
+//  Copyright © 2019 Vadim  Gorbachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
